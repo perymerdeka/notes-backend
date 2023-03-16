cecc0edd83a16346f02583173ad4ad75d4457376
perymerdeka <tkjasikk@gmail.com>
2023-03-17 02:21:03 +0700

    Done: Done Backend

--- src/apps/notes/models.py
+++ src/apps/notes/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class NotesModel(models.Model):
     title = models.CharField(max_length=255, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    color_id = models.IntegerField(max_length=3)
+    color_id = models.IntegerField()
     note_content = models.TextField()
 
     def __str__(self) -> str:
--- src/apps/notes/urls.py
+++ src/apps/notes/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
-from api.routes import router
+from apps.notes.api.routes import router
 
 urlpatterns = [
     
