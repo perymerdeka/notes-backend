8595d58d2a76e74b98de8e8becd475299a1c805c
perymerdeka <tkjasikk@gmail.com>
2023-03-25 16:08:16 +0700

    Adding Zoom Progres

---
+++ src/core/settings/production.py
@@ -0,0 +1,15 @@
+import os
+from .base import *
+
+
+# DATABASE CONFIG
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PWD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+    }
+}
